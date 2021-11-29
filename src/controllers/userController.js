1fccb85f8571932b363ae5e572d8c60ccce5f43d
Alberto Isaza <isaz@hotmail.com>
2021-11-29 11:47:10 -0500

    se inicio la validacion de token en los metodos de User - No he terminado

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,9 +2,14 @@
 const jwt = require('jsonwebtoken');
 //Importar Modulos
 const User = require('../models/user');
-const { PRIVATE_KEY } = require('./tokenController');
+const User_request = require('../models/user_request');
+const { PRIVATE_KEY, TokenController } = require('./tokenController');
 
 class UserController {
+    // Antes de fefinir las funciones genero un objeto Token Controller para asi acceder a los metodos de esa clase
+    objTokenC = new TokenController();
+
+
     //OK retorna el token con usuario autenticado
     login(req, res) {
         //Capturar datos del cuerpo de la petición
@@ -24,17 +29,41 @@ class UserController {
             }
         });
     }
-    //OK pero sin Autenticacion Necedita autenticacion
-    getAllUsers(req, res) {
-        User.find((error, data) => {
-            if (error) {
-                res.status(500).send();
-            } else {
-                res.status(200).json(data);
 
-            }
-        });
+    //OK
+    getAllUsers = (req, res) => {
+        let decode = jwt.decode(this.objTokenC.getToken(req), PRIVATE_KEY);  // este decode.id me provee el id para verificarw si es un 'admin'
+        //Insertar/crear el Usuario en la BD
+        if (decode) {
+            User.find((error, data) => {
+                if (error) {
+                    res.status(500).json({ info: error });
+                } else {
+                    res.status(201).json(data);
+                }
+            })
+        } else {
+            res.status(500).json({ info: 'Operacion No autorizada' });
+        };
     }
+    //para ser usado en el Alta de usuarios no se ha probado
+    getAllUser_requests = (req, res) => {
+        let decode = jwt.decode(this.objTokenC.getToken(req), PRIVATE_KEY);  // este decode.id me provee el id para verificarw si es un 'admin'
+        //Insertar/crear el Usuario en la BD
+        if (decode) {
+            User_request.find((error, data) => {
+                if (error) {
+                    res.status(500).json({ info: error });
+                } else {
+                    res.status(201).json(data);
+                }
+            })
+        } else {
+            res.status(500).json({ info: 'Operacion No autorizada' });
+        };
+    }
+
+
     //OK pero sin Autenticacion Necedita autenticacion
     getUserById(req, res) {
         let id = req.params.id;
@@ -46,33 +75,41 @@ class UserController {
             }
         })
     }
-    // OK pero sin Autenticacion Necedita autenticacion
-    createUser(req, res) {
-        let objUser = req.body;
-        if (objUser.nick && objUser.email && objUser.user_cat && objUser.password) {
-            User.create(objUser, (error, data) => {
+    // OK 
+    createUser = (req, res) => {
+        let { nick, email, password, user_cat } = req.body;
+        let decode = jwt.decode(this.objTokenC.getToken(req), PRIVATE_KEY);  // este decode.id me provee el id para verificarw si es un 'admin'
+        //Insertar/crear el Usuario en la BD
+        if (decode) {
+            User.create({ nick, email, password, user_cat }, (error, data) => {
                 if (error) {
-                    res.status(500).send();
+                    res.status(500).json({ info: error });
                 } else {
                     res.status(201).json(data);
                 }
             });
         } else {
-            res.status(400).send();
-        }
-
+            res.status(500).json({ info: 'Operacion No autorizada' });
+        };
     }
-    //OK pero sin Autenticacion Necedita autenticacion
-    updeteUser(req, res) {
+
+    //OK pero sin probar autenticacion
+    updeteUser = (req, res) => {
         //let {id, nombre, apellido} = req.body;
         let id = req.params.id;
-        User.findByIdAndUpdate(id, req.body, (error, data) => {
-            if (error) {
-                res.status(500).send();
-            } else {
-                res.status(200).json(data);
-            }
-        });
+        let decode = jwt.decode(this.objTokenC.getToken(req), PRIVATE_KEY);  // este decode.id me provee el id para verificarw si es un 'admin'
+        //Insertar/crear el Usuario en la BD
+        if (decode) {
+            User.findByIdAndUpdate(id, req.body, (error, data) => {
+                if (error) {
+                    res.status(500).send();
+                } else {
+                    res.status(200).json(data);
+                }
+            });
+        } else {
+            res.status(500).json({ info: 'Operacion No autorizada' });
+        };
     }
     //OK pero sin Autenticacion, la peticion viene en json no en la ruta.  Necedita autenticacion
     deleteUser(req, res) {
