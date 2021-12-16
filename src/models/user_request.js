34965329a16dec5176346f8dab8234d6e920ccaa
Andrés G <andres88884@hotmail.com>
2021-12-16 16:01:28 -0500

    main - se elimina archivo user_request.js

--- src/models/user_request.js
+++
@@ -1,7 +0,0 @@
-Este archivo debe ser borrado
-
-/* ++++++++++++++++++++++++++++++++++++++++++++++++++
-
-Ya fue eliminada la coleccion de la base de datos del Proyecto  
-
-+++++++++++++++++++++++++++++++++++++++++++++++++++++ */
