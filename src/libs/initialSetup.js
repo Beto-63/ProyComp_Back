71fd1854a61ce011b6c7f8431fb61da3a3637d13
Andrés G <andres88884@hotmail.com>
2021-12-17 21:31:10 -0500

    Modelo user_cat y llenado inicial

---
+++ src/libs/initialSetup.js
@@ -0,0 +1,40 @@
+const user_catModel = require('../models/user_cat.model');
+
+module.exports = async function createRoles() {
+
+    const count = await user_catModel.estimatedDocumentCount();
+    //console.log(count);
+
+    if(count > 0){
+        return // Ya no sigue ejecutando el código de abajo
+    }else{
+
+        try {
+            
+            // Creacion de roles por defecto
+            // Promise.all ejecuta las 3 ordenes a la vez, para mayor rendimiento
+            const values = await Promise.all([
+
+                new user_catModel(
+                    {
+                        "name": "admin"
+                    }
+                ).save(),
+
+                new user_catModel(
+                    {
+                        "name": "clerk"
+                    }
+                ).save(),
+
+            ]);
+        
+            console.log(values);
+
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
+
+}
