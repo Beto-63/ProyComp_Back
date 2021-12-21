34c93556b130169594104a640a96fe338daafb7b
Andrés G <andres88884@hotmail.com>
2021-12-20 20:43:30 -0500

    Models para flujo de token y auth

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = Schema({
     nick: {
@@ -8,12 +9,27 @@ const userSchema = Schema({
         type: String
     },
     password: {
-        type: "string"
+        type: String
     },
-    user_cat: {
+    user_cat: [{
         type: String,
-        enum: ['clerk', 'admin']
-    },
-}, { collection: 'user' });
+        enum: ['admin', 'clerk']
+    }],
+}, { timestamps: true,  collection: 'user' });
+
+// Cifrar contraseña al crear el usuario   // Contraseña ingresada por el usuario al crear la cuenta
+userSchema.statics.encryptPassword = async (password) => {
+
+    const salt = await bcrypt.genSalt(10); // String generado para cifrar la contraseña
+    const hash = await bcrypt.hash(password, salt); // El hash es el dato cifrado
+    return hash; // Se guarda la contraseña cifrada
+};
+
+// Comparar contraseñas                    // contraseña guardada, contraseña nueva
+userSchema.statics.comparePassword = async (password, receivedPassword) => {  // Se recibe la contraseña que nos pasa el usuario
+
+    // Se compara la contraseña pasada por el usuario 'paswword', con la actual almacenada en BD 'this.password'
+    return await bcrypt.compare(password, receivedPassword); // Si coinciden devuelve true, sino false
+};
 
 module.exports = model('User', userSchema);
---
+++ src/models/user_catModel.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require('mongoose');
+
+const user_catSchema = Schema({
+    name: {
+        type: String,
+        enum: ['admin', 'clerk']
+    },
+}, { timestamps: true,  /*collection: 'user_cat'*/ });
+
+module.exports = model('User_Cat', user_catSchema, 'user_cat');
