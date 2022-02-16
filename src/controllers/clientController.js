4e08b020ee454b5a5ffb84e5f17c67fc8254ded6
Alberto Isaza-PMP <87019347+Beto-63@users.noreply.github.com>
2022-02-15 21:28:08 -0500

    update

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -1,23 +1,23 @@
 const Client = require('../models/client');
 
 class ClientController {
-    getAllClients = (req, res)=>{
-        Client.find({}, (error, data)=>{
-            if(error){
-                res.status(500).json({message: "Error al hacer la petición"})
-            }else{
+    getAllClients = (req, res) => {
+        Client.find({}, (error, data) => {
+            if (error) {
+                res.status(500).json({ message: "Error al hacer la petición" })
+            } else {
                 res.status(200).json(data);
             }
         })
     }
-    
+
     newClient = (req, res) => {
         let objClient = req.body;
         Client.create(objClient, (error) => {
             if (error) {
                 res.status(500).json(error)
             } else {
-                res.status(201).json({ info: "Cliente creado exitosamente" })
+                res.status(201).json(data)
             }
         })
     }
@@ -34,14 +34,14 @@ class ClientController {
 
     editClient = (req, res) => {
         let objClient = req.body
-        Client.updateOne({_id: objClient._id},{$set: req.body},(error, data) =>{
-            if(error){
-                res.status(500).json({message: error})
-            }else{
+        Client.updateOne({ _id: objClient._id }, { $set: req.body }, (error, data) => {
+            if (error) {
+                res.status(500).json({ message: error })
+            } else {
                 res.status(200).json(data)
             }
         })
-        
+
     }
 }
 module.exports = ClientController;
