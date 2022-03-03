d73a5ce64d9ab708ef708fe679c7edcb74ba2af2
Andrés G <andres88884@hotmail.com>
2022-03-03 16:54:16 -0500

    initialSetup rutas actualizadas

--- src/libs/initialSetup.js
+++ src/libs/initialSetup.js
@@ -5,12 +5,12 @@ const createRoles = async function createRoles() {
     const count = await user_catModel.estimatedDocumentCount();
     //console.log(count);
 
-    if(count > 0){
+    if (count > 0) {
         return // Ya no sigue ejecutando el código de abajo
-    }else{
+    } else {
 
         try {
-            
+
             // Creacion de roles por defecto
             // Promise.all ejecuta las 3 ordenes a la vez, para mayor rendimiento
             const values = await Promise.all([
@@ -19,8 +19,32 @@ const createRoles = async function createRoles() {
                     {
                         "name": "admin",
                         "allowed_routes": [
-                            {"url": "/verify", "method": "GET"},
-                            {"url": "/test", "method": "GET"}
+                            { "url": "/verify", "method": "GET" },
+                            //{ "url": "/test", "method": "GET" }
+                            { "url": "/stock/channels", "method": "GET" },
+                            { "url": "/user/cats", "method": "GET" },
+                            { "url": "/user/byEmail", "method": "POST" },
+                            { "url": "/users", "method": "PUT" },
+                            { "url": "/users", "method": "POST" },
+                            { "url": "/product/categories", "method": "GET" },
+                            { "url": "/stock/addQty", "method": "PUT" },
+                            { "url": "/stock/findByCatName", "method": "POST" },
+                            { "url": "/stock/adjust", "method": "PUT" },
+                            { "url": "/stock/findByCatNameChannel", "method": "POST" },
+                            { "url": "/stock/findByNameChannel", "method": "POST" },
+                            { "url": "/stock/adjust/quantity", "method": "PUT" },
+                            { "url": "/stock/adjust/reason", "method": "POST" },
+                            { "url": "/stock", "method": "POST" },
+                            { "url": "/stock/transfer", "method": "PUT" },
+                            { "url": "/stock/findByName", "method": "POST" },
+                            { "url": "/product/combo", "method": "GET" },
+                            { "url": "/product/findByCatName", "method": "POST" },
+                            { "url": "/product/info", "method": "POST" },
+                            { "url": "/product", "method": "PUT" },
+                            { "url": "/product/selectCat", "method": "POST" },
+                            { "url": "/product/selectPacketAndFill", "method": "POST" },
+                            { "url": "/product/combo", "method": "POST" },
+                            { "url": "/product", "method": "POST" },
                         ]
                     }
                 ).save(),
@@ -29,8 +53,29 @@ const createRoles = async function createRoles() {
                     {
                         "name": "clerk",
                         "allowed_routes": [
-                            {"url": "/verify", "method": "GET"},
-                            {"url": "/test", "method": "GET"}
+                            { "url": "/verify", "method": "GET" },
+                            // {"url": "/test", "method": "GET"},
+                            { "url": "/origins", "method": "GET" },
+                            { "url": "/paymentMethods", "method": "GET" },
+                            { "url": "/product/categories", "method": "GET" },
+                            { "url": "/client", "method": "POST" },
+                            { "url": "/product/findByCatName", "method": "POST" },
+                            { "url": "/product/selectCatTemp", "method": "POST" },
+                            { "url": "/sell_ticket", "method": "POST" },
+                            { "url": "/cash/deposit", "method": "POST" },
+                            { "url": "/stock/findByChannel", "method": "POST" },
+                            { "url": "/stock/findByChannel`", "method": "PUT" },
+                            { "url": "/cash/last/transaction", "method": "POST" },
+                            { "url": "/sales/byMethod", "method": "POST" },
+                            { "url": "/cash/expense/account", "method": "POST" },
+                            { "url": "/cash/deposit/account", "method": "POST" },
+                            { "url": "/cash/sellTicket/account", "method": "POST" },
+                            { "url": "/cash/lastOpen/account", "method": "POST" },
+                            { "url": "/cash/expense", "method": "POST" },
+                            { "url": "/cash/lastClose/account", "method": "POST" },
+                            { "url": "/cash/sellTickets/unaccounted", "method": "GET" },
+                            { "url": "/cash/expense/unaccounted", "method": "POST" },
+                            { "url": "/cash/deposit/unaccounted", "method": "POST" },
                         ]
                     }
                 ).save(),
@@ -45,7 +90,7 @@ const createRoles = async function createRoles() {
                 ).save(),*/
 
             ]);
-        
+
             console.log(values);
 
         } catch (error) {
